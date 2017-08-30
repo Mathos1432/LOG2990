1430e6f15e7900088600ebb6c7653a606618db94
Dyc0de <dylan.farvacque@polymtl.ca>
2017-08-30 18:58:02 -0400

    add threejs cube with render loop, add launch.json to debug the app directly in chrome need chrome debugger extension in VSCode

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CubeComponent } from './cube/cube.component';
+
+import {RenderService} from "./cube/render.service"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CubeComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [RenderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
