34441a77cc619b014f1df28256f0ad50b520868c
Mathieu Tremblay <mathieu-4.tremblay@polymtl.ca>
2017-12-23 22:03:01 -0500

    Reformat code to respect coding conventions

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,23 +5,23 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { CubeComponent } from './cube/cube.component';
 
-import {RenderService} from './cube/render.service';
-import {BasicService} from './basic.service';
+import { RenderService } from './cube/render.service';
+import { BasicService } from './basic.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CubeComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule
-  ],
-  providers: [
-    RenderService,
-    BasicService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CubeComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule
+    ],
+    providers: [
+        RenderService,
+        BasicService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/cube/render.service.ts
+++ client/src/app/cube/render.service.ts
@@ -2,110 +2,97 @@ import { Injectable } from '@angular/core';
 import * as THREE from 'three';
 import Stats = require('stats.js');
 
+const FAR_CLIPPING_PLANE = 1000;
+const NEAR_CLIPPING_PLANE = 1;
+const FIELD_OF_VIEW = 70;
+const CAMERA_Z = 400;
+
 @Injectable()
 export class RenderService {
 
-  private container: HTMLDivElement;
+    private container: HTMLDivElement;
+    private camera: THREE.PerspectiveCamera;
+    private stats: Stats;
+    private cube: THREE.Mesh;
+    private renderer: THREE.WebGLRenderer;
+    private scene: THREE.Scene;
+    public rotationSpeedX = 0.005;
+    public rotationSpeedY = 0.01;
 
-  private camera: THREE.PerspectiveCamera;
-
-  private stats: Stats;
-
-  private cube: THREE.Mesh;
-
-  private renderer: THREE.WebGLRenderer;
-
-  private scene: THREE.Scene;
-
-  private cameraZ = 400;
-
-  private fieldOfView = 70;
-
-  private nearClippingPane = 1;
-
-  private farClippingPane = 1000;
-
-  public rotationSpeedX = 0.005;
-
-  public rotationSpeedY = 0.01;
-
-  private animateCube() {
-    this.cube.rotation.x += this.rotationSpeedX;
-    this.cube.rotation.y += this.rotationSpeedY;
-  }
-
-  private createCube() {
-    const geometry = new THREE.BoxGeometry(200, 200, 200);
-
-    for (let i = 0; i < geometry.faces.length; i += 2) {
-      const hex = Math.random() * 0xffffff;
-      geometry.faces[i].color.setHex(hex);
-      geometry.faces[i + 1].color.setHex(hex);
+    private update() {
+        this.cube.rotation.x += this.rotationSpeedX;
+        this.cube.rotation.y += this.rotationSpeedY;
     }
 
-    const material = new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors, overdraw: 0.5 });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
-  }
+    private createCube() {
+        const geometry = new THREE.BoxGeometry(50, 50, 50);
 
-  private createScene() {
-    /* Scene */
-    this.scene = new THREE.Scene();
+        for (let i = 0; i < geometry.faces.length; i += 2) {
+            const randomColor = Math.random() * 0xffffff;
+            geometry.faces[i].color.setHex(randomColor);
+            geometry.faces[i + 1].color.setHex(randomColor);
+        }
 
-    /* Camera */
-    const aspectRatio = this.getAspectRatio();
-    this.camera = new THREE.PerspectiveCamera(
-      this.fieldOfView,
-      aspectRatio,
-      this.nearClippingPane,
-      this.farClippingPane
-    );
-    this.camera.position.z = this.cameraZ;
-  }
+        const material = new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors, overdraw: 0.5 });
+        this.cube = new THREE.Mesh(geometry, material);
+        this.scene.add(this.cube);
+    }
 
-  private getAspectRatio() {
-    return this.container.clientWidth / this.container.clientHeight;
-  }
+    private createScene() {
+        /* Scene */
+        this.scene = new THREE.Scene();
 
-  private startRenderingLoop() {
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setPixelRatio(devicePixelRatio);
-    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+        /* Camera */
+        const aspectRatio = this.getAspectRatio();
+        this.camera = new THREE.PerspectiveCamera(
+            FIELD_OF_VIEW,
+            aspectRatio,
+            NEAR_CLIPPING_PLANE,
+            FAR_CLIPPING_PLANE
+        );
+        this.camera.position.z = CAMERA_Z;
+    }
 
-    this.container.appendChild(this.renderer.domElement);
-    this.render();
-  }
+    private getAspectRatio() {
+        return this.container.clientWidth / this.container.clientHeight;
+    }
 
-  private render() {
-    requestAnimationFrame(() => this.render());
-    this.animateCube();
+    private startRenderingLoop() {
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setPixelRatio(devicePixelRatio);
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
 
-    this.renderer.render(this.scene, this.camera);
-    this.stats.update();
-  }
+        this.container.appendChild(this.renderer.domElement);
+        this.render();
+    }
 
-  private initStats() {
-    this.stats = new Stats();
-    this.stats.dom.style.position = 'absolute';
+    private render() {
+        requestAnimationFrame(() => this.render());
+        this.update();
+        this.renderer.render(this.scene, this.camera);
+        this.stats.update();
+    }
 
-    this.container.appendChild(this.stats.dom);
-  }
+    private initStats() {
+        this.stats = new Stats();
+        this.stats.dom.style.position = 'absolute';
+        this.container.appendChild(this.stats.dom);
+    }
 
-  public onResize() {
-    this.camera.aspect = this.getAspectRatio();
-    this.camera.updateProjectionMatrix();
+    public onResize() {
+        this.camera.aspect = this.getAspectRatio();
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+    }
 
-    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-  }
+    public initialize(container: HTMLDivElement, rotationX: number, rotationY: number) {
+        this.container = container;
+        this.rotationSpeedX = rotationX;
+        this.rotationSpeedY = rotationY;
 
-  public initialize(container: HTMLDivElement, rotationX: number, rotationY: number) {
-    this.container = container;
-    this.rotationSpeedX = rotationX;
-    this.rotationSpeedY = rotationY;
-
-    this.createScene();
-    this.createCube();
-    this.initStats();
-    this.startRenderingLoop();
-  }
+        this.createScene();
+        this.createCube();
+        this.initStats();
+        this.startRenderingLoop();
+    }
 }
